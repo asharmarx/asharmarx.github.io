d6e96d30cbe444e90dbc82b89fc47084735d56cf
asharmarx <aman.sharmarx@gmail.com>
2022-10-29 15:55:46 -0400

    Removed atoms file

--- src/atoms/appAtoms.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai";
-
-export const appTheme = atom({
-  key: "appTheme",
-  default:
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches,
-});
