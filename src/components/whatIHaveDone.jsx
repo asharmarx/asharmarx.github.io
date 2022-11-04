0a60b2a28b3cc2e84038f0cf04869f1da818e056
asharmarx <aman.sharmarx@gmail.com>
2022-11-04 14:52:08 -0400

    Updated wihd with more info

--- src/components/whatIHaveDone.jsx
+++ src/components/whatIHaveDone.jsx
@@ -23,8 +23,16 @@ const WhatIHaveDone = () => {
         <span key={wIHD.id}>
         <h1>{wIHD.title}</h1>
         <div className='d-flex'>
-            <img src='' alt={wIHD.title} />
+           {wIHD.imageLink && <span>
+            {wIHD.imageLink.map((img) => (
+                 <img style={{ margin: "0 0.5rem 0 0"}} key={img} src={img} alt={wIHD.title} />
+            )
+            )}
+            </span>}
             <p>{wIHD.description}</p>
+            <ul>
+                {wIHD.components && wIHD.components.map((comp) => <li key={comp}>{comp}</li>)}
+            </ul>
         </div>
         </span>
   )))};
