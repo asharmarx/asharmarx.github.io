aeca4228e9a96e05fc97cc6394b6215738ac61c6
asharmarx <aman.sharmarx@gmail.com>
2022-11-20 23:13:43 -0500

    Added component to render while video loads

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import NavBar from "./components/navBar";
 import WhatIHaveDone from "./components/whatIHaveDone";
 import { useState } from "react";
 import BackgroundVideo from "./backgroundVideo";
-import WrapMeSenpai, { ButtonWrapper } from "./styles/myStyles";
+import WrapMeSenpai, { ButtonWrapper, Wait4Me } from "./styles/myStyles";
 
 const App = () => {
   const [appTheme, setAppTheme] = useState(
@@ -22,28 +22,43 @@ const App = () => {
   );
   const [muteMe, setMuteMe] = useState(true);
   const [tickleMe, setTickleMe] = useState(true);
+  const [iamLoaded, setIamLoaded] = useState(false);
   const toggleTheme = () => (appTheme ? setAppTheme(false) : setAppTheme(true));
   const toggleMute = () => (muteMe ? setMuteMe(false) : setMuteMe(true));
   const toggleTickle = () =>
     tickleMe ? setTickleMe(false) : setTickleMe(true);
-
+  console.log("loaded", iamLoaded);
   return (
     <ThemeProvider theme={appTheme ? darkTheme : lightTheme}>
       <GlobalStyles />
-      <BackgroundVideo muteMe={muteMe} tickleMe={tickleMe} />
-      <WrapMeSenpai />
-      <ButtonWrapper>
-        <ThemeButton onClick={toggleTheme} appTheme={appTheme} />
-        <MuteButton onClick={toggleMute} muteMe={muteMe} />
-        <TickleButton onClick={toggleTickle} tickleMe={tickleMe} />
-      </ButtonWrapper>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/what-i-read" element={<WhatIRead />} />
-        <Route path="/what-i-have-done" element={<WhatIHaveDone />} />
-        <Route path="/*" element={<HomePage />} />
-      </Routes>
+      <BackgroundVideo
+        muteMe={muteMe}
+        tickleMe={tickleMe}
+        setLoaded={setIamLoaded}
+      />
+      {iamLoaded ? (
+        <>
+          <WrapMeSenpai />
+          <ButtonWrapper>
+            <ThemeButton onClick={toggleTheme} appTheme={appTheme} />
+            <MuteButton onClick={toggleMute} muteMe={muteMe} />
+            <TickleButton onClick={toggleTickle} tickleMe={tickleMe} />
+          </ButtonWrapper>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/what-i-read" element={<WhatIRead />} />
+            <Route path="/what-i-have-done" element={<WhatIHaveDone />} />
+            <Route path="/*" element={<HomePage />} />
+          </Routes>
+        </>
+      ) : (
+        <Wait4Me>
+          <span>
+            <h1>Wait for me....</h1>
+          </span>
+        </Wait4Me>
+      )}
     </ThemeProvider>
   );
 };
