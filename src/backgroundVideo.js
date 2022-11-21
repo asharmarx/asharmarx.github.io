82d49bd3f25df9f353683b4d1b3c6ffd71f5a172
asharmarx <aman.sharmarx@gmail.com>
2022-11-20 23:13:58 -0500

    Added onloaded data prop

--- src/backgroundVideo.js
+++ src/backgroundVideo.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { MeInTheBackDiv } from "./styles/myStyles";
 
-const BackgroundVideo = ({ muteMe, tickleMe }) => {
+const BackgroundVideo = ({ muteMe, tickleMe, setLoaded }) => {
   const refMeUp = useRef(null);
   if (refMeUp.current) {
     if (tickleMe) {
@@ -14,7 +14,13 @@ const BackgroundVideo = ({ muteMe, tickleMe }) => {
     "https://drive.google.com/uc?id=11fKiSBYtC-5q3xiuevY3Qd6m-Y-SE2vS";
   return (
     <MeInTheBackDiv>
-      <video ref={refMeUp} autoPlay loop muted={muteMe}>
+      <video
+        ref={refMeUp}
+        autoPlay
+        loop
+        muted={muteMe}
+        onLoadedData={() => setLoaded(true)}
+      >
         <source src={videoSource} type="video/mp4" />
       </video>
     </MeInTheBackDiv>
