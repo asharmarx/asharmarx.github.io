8f23e04593eadc45d60c2bc3a97ac1eeb3ca59fd
asharmarx <aman.sharmarx@gmail.com>
2022-11-30 11:43:04 -0500

    added custom link component

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { MyNavBar } from '../styles/navStyles';
+
+const SpecialLink = ({ children, to }) => {
+    const location = useLocation();
+    const youhere = location.pathname === to;
+
+    return <Link to={to}>{children} {youhere ? '😰': ''}</Link>
+}
 
 const NavBar = () => (
     <MyNavBar className='justify-content-center' activeKey='/home'>
         <Nav.Item>
-            <Link to="/">Home</Link>
+            <SpecialLink to="/">Home</SpecialLink>
         </Nav.Item>
         <Nav.Item>
-            <Link to="/what-i-read">What I Read</Link>
+            <SpecialLink to="/what-i-read">What I Read</SpecialLink>
         </Nav.Item>
         <Nav.Item>
-            <Link to="/what-i-have-done">What I Have Done</Link>
+            <SpecialLink to="/what-i-have-done">What I Have Done</SpecialLink>
+        </Nav.Item>
+        <Nav.Item>
+            <SpecialLink to="/what-i-have-heard">What I Have Heard</SpecialLink>
         </Nav.Item>
     </MyNavBar>
 );
