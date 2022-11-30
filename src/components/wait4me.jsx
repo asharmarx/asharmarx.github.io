f18b0eed336f0f51e850a5575a57782ace67bf9e
asharmarx <aman.sharmarx@gmail.com>
2022-11-30 18:18:59 -0500

    loading component for fallback

---
+++ src/components/wait4me.jsx
@@ -0,0 +1,10 @@
+import { Wait4MeWrap } from "../styles/myStyles";
+
+const Wait4Me = () => (
+    <Wait4MeWrap>
+        <span>
+        <h1>Wait for me....</h1>
+        </span>
+    </Wait4MeWrap>)
+
+export default Wait4Me;
