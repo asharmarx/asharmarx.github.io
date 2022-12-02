a12d9313325fded812f2dfe560fac57fd2e6666e
asharmarx <aman.sharmarx@gmail.com>
2022-12-02 14:24:12 -0500

    route config for highway descriptions

---
+++ src/hookudh/useMinorHighway.js
@@ -0,0 +1,56 @@
+import { Suspense } from "react";
+import { useRoutes, Navigate } from "react-router-dom";
+import Wait4Me from "../components/wait4me.jsx";
+import {
+  HomeDesc,
+  WhatIReadDesc,
+  WhatIHaveDoneDesc,
+  WhatIHaveHeardDesc,
+} from "../components/whereAmI.jsx";
+
+const useMinorHighway = () =>
+  useRoutes([
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<Wait4Me />}>
+          <HomeDesc />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/what-i-read",
+      element: (
+        <Suspense fallback={<Wait4Me />}>
+          <WhatIReadDesc />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/what-i-have-done",
+      element: (
+        <Suspense fallback={<Wait4Me />}>
+          <WhatIHaveDoneDesc />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/what-i-have-heard",
+      element: (
+        <Suspense fallback={<Wait4Me />}>
+          <WhatIHaveHeardDesc />
+        </Suspense>
+      ),
+    },
+
+    {
+      path: "*",
+      element: (
+        <Suspense fallback={<Wait4Me />}>
+          <Navigate to="/" replace />
+        </Suspense>
+      ),
+    },
+  ]);
+
+export default useMinorHighway;
