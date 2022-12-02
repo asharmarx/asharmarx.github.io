c0dfe0bca37576741f9eed890ff575de299c258b
asharmarx <aman.sharmarx@gmail.com>
2022-12-02 14:23:44 -0500

    descriptions of different highways

---
+++ src/components/whereAmI.jsx
@@ -0,0 +1,5 @@
+
+export const HomeDesc = () => <span>me</span>
+export const WhatIReadDesc = () => <span>random things i have read with summaries and sources</span>
+export const WhatIHaveDoneDesc = () => <span>random things i have done with a little about them and if possible how to do them</span>
+export const WhatIHaveHeardDesc = () => <span>collection of songs from every year since 2020 that I listened to more than 2-3 times in a row. helps me remember that time period</span>
