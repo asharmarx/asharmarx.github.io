055b31231f8d0de35bf0ae2fb1d57e947e95e098
asharmarx <aman.sharmarx@gmail.com>
2022-12-02 14:26:44 -0500

    updated nav style for mobile

--- src/styles/navStyles.js
+++ src/styles/navStyles.js
@@ -2,7 +2,18 @@ import styled from "styled-components";
 import { Nav } from "react-bootstrap";
 
 export const MyNavBar = styled(Nav)`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  padding: 2rem;
   text-decoration: none;
   font-size: 3.5rem;
   padding-bottom: 1rem;
+  gap: 5rem;
+
+  @media screen and (max-width: 850px) {
+    display: block;
+    font-size: 1.75rem;
+    padding: 1rem 0;
+  }
 `;
