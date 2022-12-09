27da9d439a35fcf0162bd7a90e105ebf696792f3
asharmarx <aman.sharmarx@gmail.com>
2022-12-09 14:57:16 -0500

    added onprogress event handler

--- src/backgroundVideo.js
+++ src/backgroundVideo.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { MeInTheBackDiv } from "./styles/myStyles";
 
-const BackgroundVideo = ({ muteMe, tickleMe, setLoaded }) => {
+const BackgroundVideo = ({ muteMe, tickleMe, setLoaded, setWait4MeWidth }) => {
   const refMeUp = useRef(null);
   if (refMeUp.current) {
     if (tickleMe) {
@@ -9,6 +9,15 @@ const BackgroundVideo = ({ muteMe, tickleMe, setLoaded }) => {
     } else {
       refMeUp.current.pause();
     }
+  }
+  const handleProgress = () => {
+    const video = refMeUp.current;
+    if (video) {
+      const loadedPercent = video.buffered.end(0) / video.duration
+      if (loadedPercent < 0.25) return setWait4MeWidth(loadedPercent * 400);
+      return setLoaded(true);
+    }
+    return null;
   }
   const videoSource =
     "https://drive.google.com/uc?id=11fKiSBYtC-5q3xiuevY3Qd6m-Y-SE2vS";
@@ -19,7 +28,7 @@ const BackgroundVideo = ({ muteMe, tickleMe, setLoaded }) => {
         autoPlay={tickleMe}
         loop
         muted={muteMe}
-        onLoadedData={() => setLoaded(true)}
+        onProgress={handleProgress}
         playsInline
       >
         <source src={videoSource} type="video/mp4" />
