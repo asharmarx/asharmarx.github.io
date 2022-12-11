62fc61d941dece82ff17f42bc4c5d55b771ec1f7
asharmarx <aman.sharmarx@gmail.com>
2022-12-11 01:37:13 -0500

    added videowidth prop. autoplay default true

--- src/backgroundVideo.js
+++ src/backgroundVideo.js
@@ -1,7 +1,13 @@
 import { useRef } from "react";
 import { MeInTheBackDiv } from "./styles/myStyles";
 
-const BackgroundVideo = ({ muteMe, tickleMe, setLoaded, setWait4MeWidth }) => {
+const BackgroundVideo = ({
+  muteMe,
+  tickleMe,
+  setLoaded,
+  setWait4MeWidth,
+  videoWidth,
+}) => {
   const refMeUp = useRef(null);
   if (refMeUp.current) {
     if (tickleMe) {
@@ -13,19 +19,19 @@ const BackgroundVideo = ({ muteMe, tickleMe, setLoaded, setWait4MeWidth }) => {
   const handleProgress = () => {
     const video = refMeUp.current;
     if (video) {
-      const loadedPercent = video.buffered.end(0) / video.duration
-      if (loadedPercent < 0.25) return setWait4MeWidth(loadedPercent * 400);
+      const loadedPercent = video.buffered.end(0) / video.duration;
+      if (loadedPercent < 0.22) return setWait4MeWidth(loadedPercent * 300);
       return setLoaded(true);
     }
     return null;
-  }
+  };
   const videoSource =
     "https://drive.google.com/uc?id=11fKiSBYtC-5q3xiuevY3Qd6m-Y-SE2vS";
   return (
-    <MeInTheBackDiv>
+    <MeInTheBackDiv style={{ width: videoWidth }}>
       <video
         ref={refMeUp}
-        autoPlay={tickleMe}
+        autoPlay
         loop
         muted={muteMe}
         onProgress={handleProgress}
