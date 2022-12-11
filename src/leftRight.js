df2d27b367079706a0c65c4dadb507d41a393083
asharmarx <aman.sharmarx@gmail.com>
2022-12-11 01:37:33 -0500

    left right layout app

---
+++ src/leftRight.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { WrapMeSenpai, WhereAmIWrap } from "./styles/myStyles";
+import NavBar from "./components/navBar";
+import BackgroundVideo from "./backgroundVideo";
+import Wait4Me from "./components/wait4meS";
+import useMajorHighway from "./hookudh/useMajorHighway";
+import useMinorHighway from "./hookudh/useMinorHighway";
+import { LeftDiv, RightDiv, LeftRightNav } from "./styles/myStyles";
+
+const LeftRightApp = ({ muteMe, tickleMe }) => {
+  const majorHighways = useMajorHighway();
+  const minorHighways = useMinorHighway();
+
+  const [iamLoaded, setIamLoaded] = useState(false);
+  const [wait4MeWidth, setWait4MeWidth] = useState(0);
+  return (
+    <div style={{ height: "90vh", overflowY: "scroll" }}>
+      <LeftDiv>
+        {!iamLoaded && <Wait4Me wait4MeWidth={wait4MeWidth} />}{" "}
+        <BackgroundVideo
+          muteMe={muteMe}
+          tickleMe={tickleMe}
+          setLoaded={setIamLoaded}
+          setWait4MeWidth={setWait4MeWidth}
+          videoWidth="30vw"
+        />
+        <WrapMeSenpai />
+        <NavBar navWrap={LeftRightNav} />
+        <WhereAmIWrap>{minorHighways}</WhereAmIWrap>
+      </LeftDiv>
+      <RightDiv>{majorHighways}</RightDiv>
+    </div>
+  );
+};
+
+export default LeftRightApp;
