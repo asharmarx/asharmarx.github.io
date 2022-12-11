62e5fc4d84951c4cc1254307392e5fc2c8e7fa58
asharmarx <aman.sharmarx@gmail.com>
2022-12-11 01:37:41 -0500

    topbottom layout app

---
+++ src/topBottom.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import {
+  WrapMeSenpai,
+  WhereAmIWrap,
+  TopDiv,
+  BottomDiv,
+  TopBottomNav,
+} from "./styles/myStyles";
+import NavBar from "./components/navBar";
+import BackgroundVideo from "./backgroundVideo";
+import Wait4Me from "./components/wait4meS";
+import useMajorHighway from "./hookudh/useMajorHighway";
+import useMinorHighway from "./hookudh/useMinorHighway";
+
+const TopBottomApp = ({ muteMe, tickleMe }) => {
+  const majorHighways = useMajorHighway();
+  const minorHighways = useMinorHighway();
+  const [iamLoaded, setIamLoaded] = useState(false);
+  const [wait4MeWidth, setWait4MeWidth] = useState(0);
+  return (
+    <>
+      {!iamLoaded && <Wait4Me wait4MeWidth={wait4MeWidth} />}
+      <BackgroundVideo
+        muteMe={muteMe}
+        tickleMe={tickleMe}
+        setLoaded={setIamLoaded}
+        setWait4MeWidth={setWait4MeWidth}
+        style={{ width: "100%" }}
+      />
+      <WrapMeSenpai />
+      <TopDiv>
+        <NavBar navWrap={TopBottomNav} />
+        <WhereAmIWrap>{minorHighways}</WhereAmIWrap>
+      </TopDiv>
+      <BottomDiv>{majorHighways}</BottomDiv>
+    </>
+  );
+};
+
+export default TopBottomApp;
