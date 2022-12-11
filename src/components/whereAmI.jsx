d27b39632fce766070442ee5cefeec31185c0d70
asharmarx <aman.sharmarx@gmail.com>
2022-12-11 01:39:09 -0500

    updated whatihaveheard desc

--- src/components/whereAmI.jsx
+++ src/components/whereAmI.jsx
@@ -2,4 +2,4 @@
 export const HomeDesc = () => <span>me</span>
 export const WhatIReadDesc = () => <span>random things i have read with summaries and sources</span>
 export const WhatIHaveDoneDesc = () => <span>random things i have done with a little about them and if possible how to do them</span>
-export const WhatIHaveHeardDesc = () => <span>collection of songs from every year since 2020 that I listened to more than 2-3 times in a row. helps me remember that time period</span>
+export const WhatIHaveHeardDesc = () => <span>collection of songs from every year since 2020 that I listened to more than 2-3 times in a row. brings back memories from that time period and helps me remember it</span>
