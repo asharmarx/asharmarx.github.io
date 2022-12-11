ece9783ddef698a8c080159a0b7aa0d36fa13677
asharmarx <aman.sharmarx@gmail.com>
2022-12-11 01:35:40 -0500

    added functionality to switch app layouts

--- src/App.js
+++ src/App.js
@@ -1,58 +1,40 @@
-import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./styles/globalStyles";
-import { lightTheme, darkTheme } from "./themes/theme";
 import {
-  WrapMeSenpai,
-  ButtonWrapper,
-  WhereAmIWrap,
-  TopDiv,
-  BottomDiv,
-} from "./styles/myStyles";
-import {
-  ThemeButton,
   MuteButton,
+  SwitchARooButton,
+  ThemeButton,
   TickleButton,
-} from "./components/toggleButtons.jsx";
-import NavBar from "./components/navBar";
-import BackgroundVideo from "./backgroundVideo";
-import Wait4Me from "./components/wait4meS";
-import useMajorHighway from "./hookudh/useMajorHighway";
+} from "./components/toggleButtons";
 import useToggle from "./hookudh/useToggle";
-import useMinorHighway from "./hookudh/useMinorHighway";
+import LeftRightApp from "./leftRight";
+import { GlobalStyles } from "./styles/globalStyles";
+import { ButtonWrapper } from "./styles/myStyles";
+import { lightTheme, darkTheme } from "./themes/theme";
+import TopBottomApp from "./topBottom";
 
 const App = () => {
-  const majorHighways = useMajorHighway();
-  const minorHighways = useMinorHighway();
   const [appTheme, toggleAppTheme] = useToggle(
     window.matchMedia &&
       window.matchMedia("(prefers-color-scheme: dark)").matches
   );
   const [muteMe, toggleMuteMe] = useToggle(true);
   const [tickleMe, toggleTickleMe] = useToggle(true);
-  const [iamLoaded, setIamLoaded] = useState(false);
-  const [wait4MeWidth, setWait4MeWidth] = useState(0);
+  const [switchARoo, toggleSwitchARoo] = useToggle(true);
+
   return (
     <ThemeProvider theme={appTheme ? darkTheme : lightTheme}>
       <GlobalStyles />
-      {!iamLoaded && <Wait4Me wait4MeWidth={wait4MeWidth} />}
-      <BackgroundVideo
-        muteMe={muteMe}
-        tickleMe={tickleMe}
-        setLoaded={setIamLoaded}
-        setWait4MeWidth={setWait4MeWidth}
-      />
-      <WrapMeSenpai />
-      <TopDiv>
-        <ButtonWrapper>
-          <ThemeButton onClick={toggleAppTheme} appTheme={appTheme} />
-          <MuteButton onClick={toggleMuteMe} muteMe={muteMe} />
-          <TickleButton onClick={toggleTickleMe} tickleMe={tickleMe} />
-        </ButtonWrapper>
-        <NavBar />
-        <WhereAmIWrap>{minorHighways}</WhereAmIWrap>
-      </TopDiv>
-      <BottomDiv>{majorHighways}</BottomDiv>
+      <ButtonWrapper>
+        <ThemeButton onClick={toggleAppTheme} appTheme={appTheme} />
+        <MuteButton onClick={toggleMuteMe} muteMe={muteMe} />
+        <TickleButton onClick={toggleTickleMe} tickleMe={tickleMe} />
+        <SwitchARooButton onClick={toggleSwitchARoo} switchARoo={switchARoo} />
+      </ButtonWrapper>
+      {switchARoo ? (
+        <LeftRightApp muteMe={muteMe} tickleMe={tickleMe} />
+      ) : (
+        <TopBottomApp muteMe={muteMe} tickleMe={tickleMe} />
+      )}
     </ThemeProvider>
   );
 };
