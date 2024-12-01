482f26cbb21e947a42fdfcc9846f47f086c71f80
asharmarx <aman.sharmarx@gmail.com>
2024-11-30 22:43:18 -0500

    Create kubeadm.sh

---
+++ kubeadm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+# Add Docker's official GPG key:
+echo "INSTALLING CONTAINERD"
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install containerd.io
+
+echo "CONTAINERD DONE"
+echo "INSTALLING kubeadm, kubelet, kubectl"
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+echo "DONE INSTALLING kubeadm, kubelet, kubectl"
+
+echo "INIT-ING CLUSTER"
+kubeadm init --control-plane-endpoint kalima.local.mandhuk.com
+
+echo "DONE INITTING CLUSTER"
+echo "SETTING UP kubectl config"
+mkdir -p "$HOME"/.kube
+cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
